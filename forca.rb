3ebf7131e0144a850af46a3955a1a0b373e5af63
mateusgpontes <mateus.gpontes@hotmail.com>
2019-07-26 14:28:34 -0300

    adjust code

--- forca.rb
+++ forca.rb
@@ -28,9 +28,9 @@ def jogar_novamente?
   end
 end
 
-def pede_chute(chutes, erros)
+def pede_chute(chutes, erros, tenta)
   puts "\n"
-  puts "Erros até agora #{erros}"
+  puts "Erros até agora #{erros}/#{tenta}"
   puts "\n"
   puts "Chutes até agora #{chutes}"
   puts "\n"
@@ -43,6 +43,16 @@ def pede_chute(chutes, erros)
   chute
 end
 
+def conta_letra(palavra, letra)
+  total = 0
+  for caractere in palavra.chars
+    if caractere == letra
+      total = total + 1
+    end
+  end
+  total
+end
+
 def jogar(nome)
 
   palavra_secreta = escolhe_palavra_secreta
@@ -53,29 +63,32 @@ def jogar(nome)
   pontos = 0
 
   while erros < tentativa
-    chute = pede_chute(chutes, erros)
+    chute = pede_chute(chutes, erros, tentativa)
+
+    if chutes.include?(chute)
+      puts "Você já chutou essa letra"
+      next
+    end
+
     chutes << chute
+
     chutou_uma_letra = chute.size == 1
 
     if chutou_uma_letra
       procura_letra = chute[0]
-      total_encontrado = 0
-            
-      for i in 0..(palavra_secreta.size - 1)
-        if palavra_secreta[i] == procura_letra
-          pontos = pontos + 5
-          total_encontrado = total_encontrado + 1
-        end
-      end
-
-      if total_encontrado != 0
-        puts "Você ganhou 5 pontos por cada letra encontrada"
-        puts "Foram encontradas #{total_encontrado} letras."
-      else    
+      total_encontrado = conta_letra(palavra_secreta, procura_letra) 
+      #palavra_secreta.count(procura_letra)
+      
+      if total_encontrado == 0
         puts "Nenhuma letra encontrada"
         puts "Você perdeu 5 pontos"
         pontos = pontos - 5
         erros = erros + 1
+      else
+        pontos = pontos + 5
+        puts "Você ganhou 5 pontos por cada"
+        puts "Foram encontradas #{total_encontrado} letras."
+        puts "\n"
       end
 
     elsif chute == palavra_secreta
@@ -92,7 +105,7 @@ def jogar(nome)
       erros = erros + 1           
     end   
       puts "Você tem #{pontos} pontos até agora"
-    end
+  end
 end
 
 nome = boas_vindas
